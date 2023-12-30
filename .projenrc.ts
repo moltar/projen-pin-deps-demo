ead32bf85c7466aaff6e499d7002c8ddb29b6769
Roman Filippov <rf@romanfilippov.com>
2023-12-30 12:38:48 +0000

    chore: example

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,20 @@
 import { javascript, typescript } from 'projen';
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'projen-pin-deps-demo',
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
+  devDeps: [
+    '@aws-sdk/client-s3',
+    '@aws-sdk/client-sqs',
+  ],
 });
+
+project
+  .deps
+  .all
+  .filter((dep) =>dep.name.startsWith('@aws-sdk/'))
+  .map(dep => project.deps.addDependency(`${dep.name}@3.484.0`, dep.type));
+
 project.synth();
